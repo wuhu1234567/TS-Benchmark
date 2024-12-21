000bfca4c55300c2f6dcf2faa3979b369e97f2c5
wuhu1234567 <19511689233@163.com>
2024-12-21 16:42:29 +0800

    Fix: Resolve test version mismatch and update JDBC driver

--- README.md
+++ README.md
@@ -1,32 +1,8 @@
-# TS-Benchmark: A Benchmark for Time Series Databases
+# TS-Benchmark
 
 #### Description
 
-This project is the source code of TS-Benchmark. Related work has been published in ICDE 2021.
-bibTex:
-```
-@inproceedings{DBLP:conf/icde/HaoQCLSTZD21,
-  author    = {Yuanzhe Hao and
-               Xiongpai Qin and
-               Yueguo Chen and
-               Yaru Li and
-               Xiaoguang Sun and
-               Yu Tao and
-               Xiao Zhang and
-               Xiaoyong Du},
-  title     = {TS-Benchmark: {A} Benchmark for Time Series Databases},
-  booktitle = {37th {IEEE} International Conference on Data Engineering, {ICDE} 2021,
-               Chania, Greece, April 19-22, 2021},
-  pages     = {588--599},
-  publisher = {{IEEE}},
-  year      = {2021},
-  url       = {https://doi.org/10.1109/ICDE51399.2021.00057},
-  doi       = {10.1109/ICDE51399.2021.00057},
-  timestamp = {Mon, 28 Jun 2021 10:16:44 +0200},
-  biburl    = {https://dblp.org/rec/conf/icde/HaoQCLSTZD21.bib},
-  bibsource = {dblp computer science bibliography, https://dblp.org}
-}
-```
+This project is the source code of TS-Benchmark.
 
 #### Start
 
@@ -57,12 +33,11 @@ Since each database have different build-in tools for data import, we have defin
 The configuration of TSDBs is shown as follows:
 
 - InfluxDB. We enlarge the default values of some important parameters of the TSM engine for better performance of the system. For example, the parameter wal-fsync- delay is set as "0s", the parameter ``cache-max-memory-size`` is set to 1,048,576,000 bytes, and the parameter cache-snapshot-memory-size is enlarged to “100M” and so on. Maximum memory size is sufficient. The parameter ``max-values-per-tag`` is set as 0 to allow an unlimited number of tag values.
+
 - TimescaleDB. The parameter ``shared-buffers`` is set as 8GB, ``maintenance-work-mem`` is set as 2GB, ``checkpoint-completion-target`` is set as 0.7, ``min_wal`` size is set as 1GB, and max wal size is 2GB. Parameters of PostgreSQL is set based on PgTune.
+
 - Druid. The parameter ``Roll-up`` is set as true, and ``Granu-larity`` is set as hour. For local batch import, the parameter maxRowsPerSegment is set as 10M, maxRowsInMemory is set as 20M, and maxTotalRows is set as 100M. 
+
 - OpenTSDB. The parameter ``tsd-http-request-enable-chunked`` is enabled, ``tsd-http-request-max-chunk`` is set as 32KB, ``tsd-core-auto-create-metrics`` is set as true, and the parameter ``tsd-storage-enable-compaction`` is set to be false to improve the write performance.
-
-#### More
-
-If you have interests in the directed graph construction and the generation via random walk. you can ref to  the ```random_walk.ipynb``` [Quick Open It!](https://nbviewer.jupyter.org/github/dbiir/TS-Benchmark/blob/master/random_walk.ipynb)
 
 More information please ref to [for detail](./documents/时序评测工具使用手册.pdf)
--- tsdb-test/src/main/java/TSDBTest.java
+++ tsdb-test/src/main/java/TSDBTest.java
@@ -1,21 +1,19 @@
 import cn.edu.ruc.start.TSBM;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-
 public class TSDBTest {
     private static String dataPath = "";
-    public static Properties properties = null;
 
     public static void main(String[] args) throws Exception {
-        properties = new Properties();
-        try {
-            FileInputStream in = new FileInputStream("dbInfoConfig.properties");
-            properties.load(in);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-
+//        String dataPath = "/Users/fasape/project/tsdb-test/";
+//        String className = "cn.edu.ruc.InfluxdbAdapter";
+//        String ip = "10.77.110.226";
+//        String port = "8086";
+//        String userName = "root";
+//        String passwd = "root";
+//        TSBM.generateData(dataPath);
+//        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, false);
+//        testTimescaledb();
+//        testInfluxdb();
         System.out.println(args[0]);
         System.out.println(args[1]);
         System.out.println(args[2]);
@@ -75,116 +73,26 @@ public class TSDBTest {
                 testDruid(false);
             }
         }
-        if ("6".equals(args[0])) {
-            if ("0".equals(args[1])) {
-                TSBM.generateData(dataPath);
-            }
-            if ("1".equals(args[1])) {
-                testGaussDBForInfluxdb(true);
-            }
-            if ("2".equals(args[1])) {
-                testGaussDBForInfluxdb(false);
-            }
-        }
 
-        if ("7".equals(args[0])) {
-            if ("0".equals(args[1])) {
-                TSBM.generateData(dataPath);
-            }
-            if ("1".equals(args[1])) {
-                testTDEngine(true);
-            }
-            if ("2".equals(args[1])) {
-                testTDEngine(false);
-            }
-        }
-        if ("8".equals(args[0])) {
-            if ("0".equals(args[1])) {
-                TSBM.generateData(dataPath);
-            }
-            if ("1".equals(args[1])) {
-                testAliHiTSDB(true);
-            }
-            if ("2".equals(args[1])) {
-                testAliHiTSDB(false);
-            }
-        }
-        if ("9".equals(args[0])) {
-            if ("0".equals(args[1])) {
-                TSBM.generateData(dataPath);
-            }
-            if ("1".equals(args[1])) {
-                testAliLindorm(true);
-            }
-            if ("2".equals(args[1])) {
-                testAliLindorm(false);
-            }
-        }
-        if ("10".equals(args[0])) {
-            if ("0".equals(args[1])) {
-                TSBM.generateData(dataPath);
-            }
-            if ("1".equals(args[1])) {
-                testAliInfluxDB(true);
-            }
-            if ("2".equals(args[1])) {
-                testAliInfluxDB(false);
-            }
-        }
-        if ("11".equals(args[0])) {
-            if ("0".equals(args[1])) {
-                TSBM.generateData(dataPath);
-            }
-            if ("1".equals(args[1])) {
-                testKdbPlus(true);
-            }
-            if ("2".equals(args[1])) {
-                testKdbPlus(false);
-            }
-        }
-    }
-
-    private static void testKdbPlus(boolean loadParam) {
-
-        String className = "cn.edu.ruc.KdbPlusAdapter";
-        String ip = properties.getProperty("KdbPlus_ip");
-        String port = properties.getProperty("KdbPlus_port");
-        String userName = properties.getProperty("KdbPlus_username");
-        String passwd = properties.getProperty("KdbPlus_password");
-        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
     }
 
     public static void testIotdb(boolean loadParam) {
 //        String dataPath = dataPath;
-        String className = "cn.edu.ruc.IotdbAdapterNativeApi";
-        String ip = properties.getProperty("IoTDB_ip");
-        String port = properties.getProperty("IoTDB_port");
-        String userName = properties.getProperty("IoTDB_username");
-        String passwd = properties.getProperty("IoTDB_password");
+        String className = "cn.edu.ruc.IotdbAdapter";
+        String ip = "127.0.0.1";
+        String port = "6667";
+        String userName = "root";
+        String passwd = "root";
         TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
     }
 
     public static void testInfluxdb(boolean loadParam) {
 //        String dataPath = "/Users/fasape/project/tsdb-test/";
         String className = "cn.edu.ruc.InfluxdbAdapter";
-        String ip = properties.getProperty("Influx_ip");
-        String port = properties.getProperty("Influx_port");
-        String userName = properties.getProperty("Influx_username");
-        String passwd = properties.getProperty("Influx_password");
-        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
-    }
-
-    public static void testGaussDBForInfluxdb(boolean loadParam) {
-        //String dataPath = "/Users/fasape/project/tsdb-test/";
-        String istestdb = properties.getProperty("GaussdbForInflux_isTest");
-        if (!istestdb.equalsIgnoreCase("true")) {
-            return;
-        }
-        String className = "cn.edu.ruc.gaussDBForInfluxAdapter2";
-        String ip = properties.getProperty("GaussdbForInflux_ip");
-        String port = properties.getProperty("GaussdbForInflux_port");
-        String userName = properties.getProperty("GaussdbForInflux_username");
-        String passwd = properties.getProperty("GaussdbForInflux_password");
+        String ip = "127.0.0.1";
+        String port = "8086";
+        String userName = "root";
+        String passwd = "root";
         TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
     }
 
@@ -201,7 +109,7 @@ public class TSDBTest {
     public static void testOpentsdb(boolean loadParam) {
         String className = "cn.edu.ruc.OpentsdbAdapter";
         String ip = "127.0.0.1";
-        String port = "8242";
+        String port = "4242";
         String userName = "root"; //not required
         String passwd = "root"; //not required
         TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
@@ -209,47 +117,10 @@ public class TSDBTest {
 
     public static void testDruid(boolean loadParam) {
         String className = "cn.edu.ruc.DruidAdapter";
-        String ip = properties.getProperty("Druid_ip");
-        String port = properties.getProperty("Druid_port");
-        ;
-        String userName = properties.getProperty("Druid_username"); //not required
-        String passwd = properties.getProperty("Druid_password"); //not required
-        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
-    }
-
-    public static void testTDEngine(boolean loadParam) {
-        String className = "cn.edu.ruc.TdengineAdapter2";
-        String ip = properties.getProperty("TDEngine_ip");
-        String port = properties.getProperty("TDEngine_port");
-        String userName = properties.getProperty("TDEngine_username"); //not required
-        String passwd = properties.getProperty("TDEngine_password"); //not required
-        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
-    }
-
-    public static void testAliHiTSDB(boolean loadParam) {
-        String className = "cn.edu.ruc.AliHiTSDBAdapter";
-        String ip = properties.getProperty("AliHiTSDB_ip");
-        String port = properties.getProperty("AliHiTSDB_port");
-        String userName = properties.getProperty("AliHiTSDB_username");
-        String passwd = properties.getProperty("AliHiTSDB_password");
-        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
-    }
-
-    public static void testAliLindorm(boolean loadParam) {
-        String className = "cn.edu.ruc.AliLindormAdapter";
-        String ip = properties.getProperty("AliLindorm_ip");
-        String port = properties.getProperty("AliLindorm_port");
-        String userName = properties.getProperty("AliLindorm_username");
-        String passwd = properties.getProperty("AliLindorm_password");
-        TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
-    }
-
-    public static void testAliInfluxDB(boolean loadParam) {
-        String className = "cn.edu.ruc.AliInfluxAdapter";
-        String ip = properties.getProperty("AliInflux_ip");
-        String port = properties.getProperty("AliInflux_port");
-        String userName = properties.getProperty("AliInflux_username"); //not required
-        String passwd = properties.getProperty("AliInflux_password"); //not required
+        String ip = "127.0.0.1";
+        String port = "";
+        String userName = "root"; //not required
+        String passwd = "root"; //not required
         TSBM.startPerformTest(dataPath, className, ip, port, userName, passwd, false, loadParam);
     }
 }
